2fc01165246b7a0ef23831085167b461dd7fe285
isTerryC <1908238227@qq.com>
2025-01-27 21:28:57 +0800

    [v7] 分组/GameManager/randf_range()/clamp()

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -6,13 +6,28 @@ extends Area2D
 #func _ready() -> void:
 	#pass # Replace with function body.
 
+var is_dead : bool = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	position += Vector2(slime_speed,0) * delta
-
+	if not is_dead:
+		position += Vector2(slime_speed,0) * delta
+	
+	if position[0] < -250:
+		queue_free()
+		
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is CharacterBody2D:
 		body.game_over()
 		
+
+
+func _on_area_entered(area: Area2D) -> void:
+	if area.is_in_group("bullet"):
+		$AnimatedSprite2D.play("death")
+		is_dead = true
+		area.queue_free()
+		
+		await get_tree().create_timer(0.6).timeout
+		queue_free()
